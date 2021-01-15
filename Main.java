7d4208034711cf379bfb9eb08620433bf1c854c4
Shahid Ghazanfar <71063599+Gazza7d3@users.noreply.github.com>
2021-01-15 15:09:32 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.shahid;
+
+class Book{
+    String Title;
+    String Author;
+    int numberOfPages;
+
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(Book);
+
+    }
+
+
+}
+
+
